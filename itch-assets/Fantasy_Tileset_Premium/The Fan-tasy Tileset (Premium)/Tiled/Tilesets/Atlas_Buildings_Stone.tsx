c93681e5c6485516209d03608231a93d6fbe6f20
Aj@Botpress <ajaykumar.mudaliar@botpress.com>
2025-12-24 20:21:56 -0500

    clean up

--- itch-assets/Fantasy_Tileset_Premium/The Fan-tasy Tileset (Premium)/Tiled/Tilesets/Atlas_Buildings_Stone.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Atlas_Buildings_Stone" tilewidth="16" tileheight="16" tilecount="8190" columns="91">
- <image source="../../Art/Buildings/Atlas/Buildings_Stone.png" width="1456" height="1440"/>
-</tileset>
--- itch-assets/Fantasy_Tileset_Premium/The Fan-tasy Tileset (Premium)/Tiled/Tilesets/Atlas_Buildings_Wood.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Atlas_Buildings_Wood" tilewidth="16" tileheight="16" tilecount="8190" columns="91">
- <image source="../../Art/Buildings/Atlas/Buildings_Wood.png" width="1456" height="1440"/>
-</tileset>
